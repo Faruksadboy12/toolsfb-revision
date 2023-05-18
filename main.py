50e20dc4b052667f0b39bd8d13a453f6806f9a20
Faruksadboy12 <103483459+Faruksadboy12@users.noreply.github.com>
2023-05-19 00:36:37 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+########################################>
+#     *hacker badut freak 
+########################################>
+#     *Coding by Dikduk Alatafasy Freak
+#     *Tools Freak  <--- this tools fake Freak
+#     *version 1.3
+	
+#--- JALANKAN TOOL
+if __name__=="__main__":
+	try:
+		import os, sys, shutil, subprocess 
+	except requests.exceptions.ConnectionError:
+		exit ('\n\t\x1b[0m $ koneksi bermasalah ...\n')
+			
+	if sys.version_info.major != 3: # cek versi python 
+		exit("\n\t\x1b[0m type: python main.py \n")
+		
+	try: # install module
+		import requests 
+	except (ImportError,ModuleNotFoundError):
+		print ('\n\t\x1b[0m $ install module requests ...\n')
+		os.system("pip install requests")
+	try:
+		import bs4 
+	except (ImportError,ModuleNotFoundError):
+		print ('\n\t\x1b[0m $ install module bs4 ...\n')
+		os.system("pip install bs4")
+	try:
+		import mechanize 
+	except (ImportError,ModuleNotFoundError):
+		print ('\n\t\x1b[0m $ install module mechanize ...\n')
+		os.system("pip install mechanize")
+	try:
+		import concurrent.futures 
+	except (ImportError,ModuleNotFoundError):
+		print ('\n\t\x1b[0m $ install module futures ...\n')
+		os.system("pip install futures")
+	try:
+		import rich 
+	except (ImportError,ModuleNotFoundError):
+		print ('\n\t\x1b[0m $ install module rich ...\n')
+		os.system("pip install rich")
+	 
+	from freak.menu import aku_aneh_aku_ini_aneh as black sadboyxd 
+	#from log.masuk import alatafasy_code as masuk 
+	from shutil import rmtree as lolichan
+	runtah = [
+		"romz/__pycache__",
+		"log/__pycache__",
+		"xyz/__pycache__"
+	]
+	try:
+		[freaksad(xz) for xz in freak]
+	except: 
+		pass 
+	os.system('git pull')
+	black sadboyxd()
